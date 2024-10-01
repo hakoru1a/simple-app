e38ce8bcb8a117af3824030ab242e9acb3695bc7
hakoru1a <hakoru1a@gmail.com>
2024-10-01 12:58:50 +0700

    init src

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const os = require('os');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send(
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+  );
+});
+
+app.post('/', (req, res) => {
+  res.send({
+    data: `this is secrect data from ${os.hostname()}`
+  });
+});
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
